313e5a240b2d61ece923a794a9264e81a0c80fa3
Ceci Villaseñor <m312723@alunos.ipb.pt>
2023-03-14 01:06:03 +0000

    Character Controller and Keyboard Input

--- Assets/Scripts/RubyController.cs
+++ Assets/Scripts/RubyController.cs
@@ -4,14 +4,19 @@ using UnityEngine;
 
 public class RubyController : MonoBehaviour
 {
+    float horizontal;
+    float vertical;
     void Start()
     {
-        
     }
     void Update()
     {
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
+
         Vector2 position = transform.position;
-        position.x = position.x + 0.1f;
+        position.x = position.x + 3.0f * horizontal * Time.deltaTime;
+        position.y = position.y + 3.0f * vertical * Time.deltaTime;
         transform.position = position;
     }
 }
