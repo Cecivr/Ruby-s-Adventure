804f9c94e39db9359a88daeacf4e0276ccfea231
Ceci Villaseñor <m312723@alunos.ipb.pt>
2023-03-15 11:45:34 +0000

    World Interactions - Blocking Movement

--- Assets/Scripts/RubyController.cs
+++ Assets/Scripts/RubyController.cs
@@ -4,19 +4,24 @@ using UnityEngine;
 
 public class RubyController : MonoBehaviour
 {
+    Rigidbody2D rigidbody2d;
     float horizontal;
     float vertical;
     void Start()
     {
+        rigidbody2d = GetComponent<Rigidbody2D>();
     }
     void Update()
     {
         horizontal = Input.GetAxis("Horizontal");
         vertical = Input.GetAxis("Vertical");
-
-        Vector2 position = transform.position;
+    }
+    void FixedUpdate()
+    {
+        Vector2 position = rigidbody2d.position;
         position.x = position.x + 3.0f * horizontal * Time.deltaTime;
         position.y = position.y + 3.0f * vertical * Time.deltaTime;
-        transform.position = position;
+
+        rigidbody2d.MovePosition(position);
     }
 }
