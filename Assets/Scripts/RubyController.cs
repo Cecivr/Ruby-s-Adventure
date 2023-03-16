1f153909624c3e509e4741becf385181c9476caf
Ceci Villaseñor <m312723@alunos.ipb.pt>
2023-03-16 17:15:57 +0000

    Visual Styling - User Interface - Head-Up Display

--- Assets/Scripts/RubyController.cs
+++ Assets/Scripts/RubyController.cs
@@ -80,6 +80,8 @@ public class RubyController : MonoBehaviour
         }
 
         currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
+
+        UIHealthBar.instance.SetValue(currentHealth / (float)maxHealth);
     }
     void Launch()
     {
