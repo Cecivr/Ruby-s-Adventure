9aa1bfdaf6a080d10b9df04efae93bf385aca718
Ceci Villaseñor <m312723@alunos.ipb.pt>
2023-05-10 12:35:26 +0100

    sound

--- Assets/Scripts/DamageZone.cs
+++ Assets/Scripts/DamageZone.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class DamageZone : MonoBehaviour
 {
+    AudioSource sound;
+    private void Start()
+    {
+        sound = GetComponent<AudioSource>();
+    }
     void OnTriggerEnter2D(Collider2D other)
     {
         RubyController controller = other.GetComponent<RubyController>();
@@ -11,6 +16,7 @@ public class DamageZone : MonoBehaviour
         if (controller != null)
         {
             controller.ChangeHealth(-1);
+            sound.Play();
         }
     }
 }
